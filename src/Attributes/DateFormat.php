b714c8033add4421b8bf90a3ed6d6cb6987ef398
Chris Burton <csburton@gmail.com>
2025-07-06 10:28:58 +0100

    Adding date format attribute

---
+++ src/Attributes/DateFormat.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Pantono\Contracts\Attributes;
+
+use Attribute;
+
+#[Attribute]
+class DateFormat
+{
+    public string $name = '';
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+}
